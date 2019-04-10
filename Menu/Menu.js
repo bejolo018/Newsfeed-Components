96034711199e4ae8a8a8c80dc70a2d459f4192a1
unknown <bejolo018@gmail.com>
2019-04-10 15:06:58 -0500

    completed imports and begun work on constructors

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,26 @@
 
+// class actionMenu {
+//   constructor(menu){
+//     this.menu = menu;
+//     this.menuButtons = this.menu.querySelector('.menu-button')
+
+//   }
+// }
+
+
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
+  const menu = document.querySelector('.menu')
+  // menu.classList.toggle('menu-open');
+  // element.classList.toggle('menu-open');
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector('.menu-button')
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click', event => {
+  event.toggleMenu();
+})
+
